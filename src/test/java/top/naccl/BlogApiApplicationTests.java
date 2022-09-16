69d3aaeb6a0d1c94219a4b0b12a2d4e538d86f86
longzhan.huang <juaneboosham@gmail.com>
2022-09-16 10:27:39 +0800

    test jenkins

---
+++ src/test/java/top/naccl/BlogApiApplicationTests.java
@@ -0,0 +1,18 @@
+package top.naccl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+	@Test
+	void test() {
+	}
+
+}
